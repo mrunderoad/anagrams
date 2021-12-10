a4da99adf111ebeef34ebc111f1e25b309d236c5
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-12-10 09:03:49 -0800

    first test good fail

---
+++ lib/anagrams.rb
@@ -0,0 +1,13 @@
+class Anagrams
+  def inititialize(text)
+    @text = text
+  end
+
+  def text=(text)
+    @text = text
+  end
+
+  def search_anagrams(word_one, word_two)
+
+  end
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,2 +1,10 @@
 require('rspec')
 require('anagrams')
+
+
+describe('Anagrams#search_anagrams') do
+  it('takes in two words and puts them into an array') do
+  words = Anagrams.new()
+  expect(words.search_anagrams("tea", "eat")).to(eq(["t","e","a","e","a","t"]))
+  end
+end
